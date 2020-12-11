f6801e3a472005288df981f00c2ce1ed7a719e82
Chase Snider <jcsnider@outlook.com>
2020-12-11 12:30:24 -0500

    styled and ready to go

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import './index.css'
+import Character from './components/Character'
 
 const App = () => {
+
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(()=>{
+    axios.get('https://rickandmortyapi.com/api/character')
+      .then((res)=>{
+        setCharacters(res.data.results)
+      })
+      .catch(err => {
+        console.log('ERROR')
+      })
+  },[])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,6 +27,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character characters={characters} />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,75 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components';
+
+export default function Character(props) {
+   
+    const {characters} = props;
+
+    return (
+        <div>
+           {characters.map(character => {
+                return (
+                    <CardStyle>
+                    <div>
+                        <img src={character.image} alt={character.name}/>
+                        <h2>{character.name}</h2>
+                        <div className= 'info'>
+                            <p>Origin: {character.origin.name}</p>
+                            <p>Gender: {character.gender}</p>
+                            <p>Species: {character.species}</p>
+                            <p>Status: {character.status}</p>    
+                        </div>  
+                    </div>
+                    </CardStyle>
+                )
+            })}
+        </div>
+    )
+}
+
+
+const CardStyle = styled.div`
+     margin: 30px auto;
+     border: 1px solid mediumpurple;
+     box-shadow: 0px 1px 40px -2px mediumpurple;
+     border-radius: 20px;
+     width: 35%;
+     background-color: mediumspringgreen;
+     font-weight: bold;
+     opacity: 0;
+     transition: .1s;
+     &:hover{
+        opacity: 1;
+
+     img {
+         margin: 20px auto;
+         height: 20vh;
+         border: 2px solid mediumpurple;
+         box-shadow: 0px 1px 10px -2px mediumpurple;
+         border-radius: 20px;
+         
+
+     }
+
+     h2 {
+         color:purple;
+        font-family:fantasy;
+        font-weight: bold;
+        text-shadow: 2px 2px orange;
+     }
+     .info {
+        border: 1px solid mediumpurple;
+        background-color: magenta;
+        border-bottom-left-radius: 20px;
+        border-bottom-right-radius: 20px;
+     }
+     .info p{
+        font-size: 1.25rem;
+        font-family:monospace;
+        text-shadow: 1px 1px mediumspringgreen;
+        
+        }
+     }
+
+`
